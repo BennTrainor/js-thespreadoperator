458b8612f9117afbf8ed55d8d7b2fd906c7e9f73
BennTrainor <>
2023-10-01 18:04:12 +0000

    add challenge solution and explanation

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -33,3 +33,47 @@ console.log('object 3: ', obj3);
 
 let arr5 = [...arr1, {...obj1 }, ...arr3, "x", "y", "z" ]
 console.log('Array 5: ', arr5);
+
+//the spread operator challenge:
+//You will use the spread operator to copy an array which is stored in an object which itself is stored in an array. 
+//You will create a single line arrow function (has no return statement), 
+//this should copy an array passed in and add the value passed to it, then return the array.
+
+//data
+
+let students = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 63, maths: 87, art: 95},
+    }
+];
+
+//solution and explanation
+
+//[...students[0].subjects] uses the spread operator to create a new array with the same elements as students[0].subjects
+let subjects = [...students[0].subjects];
+
+//(item, val) are the parameters of the arrow function.
+//=> is the arrow notation indicating it's a function.
+//[...item, val] uses the spread operator to create a new array with the elements of item followed by val
+const update = (item, val) => [...item, val];
+
+//This will take the subjects array (which is a copy of the original students[0].subjects) and add 'Electronics' to it.
+let updatedSubjects = update(subjects, 'Electronics');
+
+console.log(updatedSubjects);
+console.log(students[0]);
